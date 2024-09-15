6cda657f1b17d457a72582ece0d986ae2f794f8c
Andrew Kelsey <ajkelsey@grandroyal.org>
2024-09-14 23:05:01 -0400

    Script update

--- speedcam-st.sh
+++ speedcam-st.sh
@@ -30,14 +30,6 @@ echo "under certain conditions; type 'cat LICENSE' for details."
 echo -e "\n\n"
 echo -e "Usage: $(basename "$0") { install | uninstall }\n"
 
-# Check if running as root
-if [ "$(id -u)" -eq 0 ]; then
-    echo -e "Do not run as root.\n"
-    exit
-fi
-
-
-
 function install () {
     user=$(whoami)
     root='/opt/speedcam'
@@ -97,6 +89,13 @@ function uninstall () {
 
 }
 
+# Check if running as root
+if [ "$(id -u)" -eq 0 ]; then
+    echo -e "Do not run as root.\n"
+    exit
+fi
+
+# Parse command line arguments
 if [ "${1}" == "install" ]; then
     install
 elif [ "${1}" == "uninstall" ]; then
